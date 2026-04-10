b13000d88cd29f7a7f5dc22cb048c99c559435a3
H4YI3R <whguswns1977@gmail.com>
2026-04-10 11:40:23 +0900

    F

--- BetaPage/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer style={{ padding: "1rem", background: "#eee", marginTop: "2rem" }}>
-            <p>&copy; 2026 My React App</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- BetaPage/src/routes/index.jsx
+++
@@ -1,9 +0,0 @@
-import Home from '@pages/Home.jsx';
-import About from '@pages/About.jsx';
-
-const routes = [
-    { path: '/', element: <Home /> },
-    { path: '/about', element: <About /> },
-];
-
-export default routes;
